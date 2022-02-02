beea00299035a152a77bbad7ca9112ee35928fd2
LiangShuang <liangshuang@sutu.tech>
2022-02-02 16:54:20 +0800

    fix digits

--- src/services/filters.ts
+++ src/services/filters.ts
@@ -1,4 +1,4 @@
-export function demojo(mojo: null | number, unit = "XCH", decimal = 3, digits = 4) {
+export function demojo(mojo: null | number, unit = "XCH", decimal = 12, digits = 6) {
     if (!mojo) return "0.0 " + unit;
     return Number((mojo / Math.pow(10, decimal)).toFixed(digits)) + " " + unit;
 }
