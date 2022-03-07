d3b757e0d56762522dbc056381bb3ee84d475ea4
LiangShuang <liangshuang@sutu.tech>
2022-03-07 16:14:14 +0800

    init popup entrance

--- src/entry/popup.ts
+++ src/entry/popup.ts
@@ -1,8 +1,34 @@
-import Vue from 'vue'
+import Vue from "vue";
+import router from "@/router";
+import store from "@/store";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import i18n from "@/i18n/i18n";
+import "@mdi/font/css/materialdesignicons.css";
 import App from '../views/popup.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Buefy);
+
+(Vue as any).$storeType = 'background';
+console.log((Vue as any).$storeType)
+
+document.title = "Pawket";
 
 new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+  router,
+  store,
+  i18n,
+  render: (h) => h(App),
+}).$mount("#app");
+
+import "@/store/modules/app";
+import "@/store/modules/account";
+import "@/store/modules/vault";
+import "@/store/modules/network";
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
+store.dispatch("initState");
--- src/views/popup.vue
+++ src/views/popup.vue
@@ -1,29 +1,90 @@
 <template>
-  <div class="main_app">
-    <h1>Hello {{msg}}</h1>
+  <div id="app">
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="@/assets/logo.svg" :alt="$t('verifyPassword.ui.alt.logoAlt')" />
+        </b-navbar-item>
+      </template>
+      <template #start> </template>
+      <template #end>
+        <b-navbar-dropdown :label="$t('app.ui.button.lang')">
+          <b-navbar-item @click="changeLang('en')" :active="$i18n.locale === 'en'"> English </b-navbar-item>
+          <b-navbar-item @click="changeLang('zhcn')" :active="$i18n.locale === 'zhcn'"> 简体中文 </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { NotificationProgrammatic as Notification } from "buefy";
+import store from "@/store";
+import DevHelper from "@/components/DevHelper.vue";
+import UniStorage from '@/services/storage';
 
-<script>
-export default {
-  name: 'popupView',
-  data () {
-    return {
-      msg: 'popup'
+@Component
+export default class App extends Vue {
+  public debugClick = 9;
+  get version(): string {
+    return process.env.VUE_APP_VERSION || "";
+  }
 
+  get debugMode(): boolean {
+    return store.state.app.debug;
+  }
+
+  alphaClick(): void {
+    this.debugClick--;
+    if (this.debugClick == 0) {
+      store.state.app.debug = true;
+      Notification.open({
+        message: `Debug mode enabled`,
+        type: "is-success",
+      });
     }
   }
-}
 
+  async changeLang(lang: string):Promise< void> {
+    this.$i18n.locale = lang;
+    await UniStorage.create().setItem("Locale", lang);
+  }
+
+  disableDebug(): void {
+    this.debugClick = 9;
+    store.state.app.debug = false;
+    Notification.open({
+      message: `Debug mode disabled`,
+      type: "is-success",
+    });
+  }
+
+  showDebugHelper(): void {
+    this.$buefy.modal.open({
+      parent: this,
+      component: DevHelper,
+      hasModalCard: true,
+      trapFocus: true,
+      props: {},
+    });
+  }
+}
 </script>
 
-<style>
-.main_app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body,
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  min-width: 400px;
+  min-height: 600px;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
 }
 </style>
