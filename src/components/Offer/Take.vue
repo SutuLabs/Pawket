b1b695e989d957cfcf16ea9a9beb8b2f6079e3b0
LiangShuang <liangshuang@sutu.tech>
2022-03-20 15:55:28 +0800

    take offer parse offer

--- src/components/Offer/Take.vue
+++ src/components/Offer/Take.vue
@@ -11,18 +11,27 @@
       <b-field v-if="summary" label="Information">
         <template #message>
           <ul v-for="(arr, sumkey) in summary" :key="sumkey" :class="sumkey">
-            <li>{{ sumkey }}</li>
-            <li class="pt-1" v-for="(ent, idx) in arr" :key="idx">
-              <b-taglist attached>
-                <b-tag v-if="ent.id && cats[ent.id]" type="is-info" :title="cats[ent.id] + '(' + ent.id + ')'">{{
-                  cats[ent.id]
-                }}</b-tag>
-                <b-tag v-else-if="ent.id" type="is-info" :title="ent.id">CAT {{ ent.id.slice(0, 7) + "..." }}</b-tag>
-                <b-tag v-else type="is-info">XCH</b-tag>
+            <li v-if="sumkey == 'requested'">In exchange for</li>
+            <li v-if="sumkey == 'offered'">You will receive</li>
+            <li>
+              <ol class="token-list">
+                <li class="pt-1" v-for="(ent, idx) in arr" :key="idx">
+                  <b-taglist attached>
+                    <b-tag v-if="ent.id && cats[ent.id]" type="is-info" :title="cats[ent.id] + ' (' + ent.id + ')'">{{
+                      cats[ent.id]
+                    }}</b-tag>
+                    <b-tag v-else-if="ent.id" type="is-info" :title="ent.id">CAT {{ ent.id.slice(0, 7) + "..." }}</b-tag>
+                    <b-tag v-else type="is-info" title="Original XCH">XCH</b-tag>
 
-                <b-tag type="">{{ ent.amount }}</b-tag>
-                <b-tag type="is-info is-light" :title="ent.target">{{ ent.target.slice(0, 7) + "..." }}</b-tag>
-              </b-taglist>
+                    <b-tag class="" :title="ent.amount + ' mojos'">{{
+                      ent.amount | demojo(ent.id && tokenInfo[cats[ent.id]])
+                    }}</b-tag>
+                    <b-tag v-if="sumkey == 'requested'" type="is-info is-light" :title="ent.target">{{
+                      ent.target.slice(0, 10) + "..."
+                    }}</b-tag>
+                  </b-taglist>
+                </li>
+              </ol>
             </li>
           </ul>
         </template>
@@ -58,27 +67,29 @@
 import { Component, Vue, Prop, Emit } from "vue-property-decorator";
 import KeyBox from "@/components/KeyBox.vue";
 import { SpendBundle } from "@/models/wallet";
-import offer, { OfferSummary } from "@/services/transfer/offer";
-import { AccountEntity } from "@/store/modules/account";
-import { prefix0x } from "@/services/coin/condition";
+import offer, { getCatIdDict, getCatNameDict, getTokenInfo, OfferSummary } from "@/services/transfer/offer";
+import { AccountEntity, TokenInfo } from "@/store/modules/account";
+import { demojo } from "@/filters/unitConversion";
 
 @Component({
   components: {
     KeyBox,
   },
+  filters: { demojo },
 })
 export default class TakeOffer extends Vue {
   @Prop() private account!: AccountEntity;
-  @Prop() private inputOfferText!: string;
+  @Prop({
+    default:
+      "offer1qqp83w76wzru6cmqvpsxygqqama36ngn6fhxtlen3xthnqm0s83gfww3vu3ld0vat8yt6ukl6tlwnhpswkcxq8dduarwhww30fhtf8sm4hnsdw57qvthkzu2acw5s6hhh9e8n4vwfy9jdfr493ww02xdequ5xnmultpg9dfhhthaeqf2ewf3qlt8lnfrj00038u39m26u5umc8r2zd2p48su46g8lemd2sne6rfdg3r7knncj78pzg4nvzr99k5hq5nnf87vadw0dk57a4y0tamaxvwvl0dsnygj873h9geldnel5zfqjat8es0vthvmjfmywasn0ldcmarw4lqzae7q44qm7f0h987vjk3npk2gvzf2qfvhwfflhex74d0yutlnm4dm3dtcjnllmf42ed7rehvrvcxj7yau0ge7kygryq662le6x4xkcwua7rfk0rplvscaahta9ajf749znz05lnl2zxwy66lvxemxdhtpvr5kh7tea8nrew48qt25tjf8ank9dwty2hf06dttp4sxfz90a0mq6u3u0f8h00pl5wlh2jvmpn6cymv0kstzmm8vw0ej408jm2h68xlmlu3lxzqcr4q333j3dgp6l0lskdhu0hjlpm6djr75duff5kannl7897tglhan38nadmumw0ng40rj20uqkr4p48a2ax77mk088ekxx6h6c6a4nafy2eadhzteej07ls6el7fhheackkjdyuncs2k0zyf0mwer59n05s2hz9rdz4l8nnkd3sjk4tremwtjw5mf82n45w57rec06a2ejft83rmz6wmawfxl4nazg6x4lutkh0vv6979y5psq2q0hlfscgd2p5",
+  })
+  private inputOfferText!: string;
+  // @Prop() private inputOfferText!: string;
   public offerText = "";
   public offerBundle: SpendBundle | null = null;
   public summary: OfferSummary | null = null;
   public submitting = false;
   public bundle: SpendBundle | null = null;
-
-  get bundleText(): string {
-    return this.offerBundle == null ? "" : JSON.stringify(this.offerBundle);
-  }
 
   @Emit("close")
   close(): void {
@@ -86,7 +97,15 @@ export default class TakeOffer extends Vue {
   }
 
   get cats(): { [id: string]: string } {
-    return Object.assign({}, ...this.account.cats.map((_) => ({ [prefix0x(_.id)]: _.name })));
+    return getCatNameDict(this.account);
+  }
+
+  get catIds(): { [name: string]: string } {
+    return getCatIdDict(this.account);
+  }
+
+  get tokenInfo(): TokenInfo {
+    return getTokenInfo(this.account);
   }
 
   mounted(): void {
@@ -105,4 +124,8 @@ export default class TakeOffer extends Vue {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+ol.token-list {
+  margin-left: 2em;
+}
+</style>
