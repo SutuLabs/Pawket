f9eff5373c1871fa11da771d67d8d964f4e117a5
Sun Haochen <sunhaochen@sutu.tech>
2022-04-10 23:51:31 -0700

    change usd symbol

--- src/filters/usdtConversion.ts
+++ src/filters/usdtConversion.ts
@@ -1,20 +1,20 @@
 import bigDecimal from "js-big-decimal";
 
 export function xchToUsdt(mojo: null | number | bigint, rate = -1, digits = -1): string {
-  const unit = "USDT";
+  const symbol = "$";
   const decimal = 12;
   if (rate == -1) {
-    return "-1 " + unit;
+    return "-1";
   }
   digits = digits == -1 ? 12 : digits;
 
-  if (!mojo) return "0.0 " + unit;
+  if (!mojo || mojo < 0) return symbol + "0.0";
   const num = mojo.toString();
   const bnum = bigDecimal.divide(num, Math.pow(10, decimal), digits);
   const value = bigDecimal.multiply(bnum, rate);
   const fnum = value.replace(/\.?0+$/, "");
 
-  return fnum + " " + unit;
+  return symbol + fnum;
 }
 
 export default { xchToUsdt };
