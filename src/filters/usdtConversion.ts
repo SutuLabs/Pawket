2e8d0bdec6f517c3562972df4f1650168ca8c360
Sun Haochen <sunhaochen@sutu.tech>
2022-04-11 21:37:56 -0700

    round number to 2 decimals

--- src/filters/usdtConversion.ts
+++ src/filters/usdtConversion.ts
@@ -1,20 +1,21 @@
 import bigDecimal from "js-big-decimal";
 
-export function xchToUsdt(mojo: null | number | bigint, rate = -1, digits = -1): string {
+export function xchToUsdt(mojo: null | number | bigint, rate = -1): string {
   const symbol = "$";
   const decimal = 12;
   if (rate == -1) {
     return "-1";
   }
-  digits = digits == -1 ? 12 : digits;
+  const digits = 2;
 
-  if (!mojo || mojo < 0) return symbol + "0.0";
+  if (!mojo || mojo < 0) return symbol + "0.00";
   const num = mojo.toString();
   const bnum = bigDecimal.divide(num, Math.pow(10, decimal), digits);
   const value = bigDecimal.multiply(bnum, rate);
-  const fnum = value.replace(/\.?0+$/, "");
+  const fnum = bigDecimal.round(value, digits, bigDecimal.RoundingModes.HALF_UP);
+  const pnum = bigDecimal.getPrettyValue(fnum, 3, ',');
 
-  return symbol + fnum;
+  return symbol + pnum;
 }
 
 export default { xchToUsdt };
