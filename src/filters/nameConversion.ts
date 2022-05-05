7d58278868f11744fe6068b52c2adc5cf8af84dc
Sun Haochen <sunhaochen@sutu.tech>
2022-05-04 20:42:27 -0700

    fix if condition

--- src/filters/nameConversion.ts
+++ src/filters/nameConversion.ts
@@ -1,6 +1,6 @@
 export function nameOmit(name: string, ellipsis = "...", length = 10): string {
   if (!name) return "";
-  if (name.length <= length) return name;
+  if (name.length <= length + ellipsis.length) return name;
   return name.slice(0, length) + ellipsis;
 }
 
