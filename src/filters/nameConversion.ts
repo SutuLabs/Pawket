b2af167f89bc2e2ee8e47779bb28dfc8dd401d81
Sun Haochen <sunhaochen@sutu.tech>
2022-05-06 11:13:45 -0700

    add uppercase parameter

--- src/filters/nameConversion.ts
+++ src/filters/nameConversion.ts
@@ -1,4 +1,4 @@
-export function nameOmit(name: string, ellipsis = "...", length = 10): string {
+export function nameOmit(name: string, ellipsis = "...", length = 10, uppercase = true): string {
   let nameRes = "";
   if (!name) return nameRes;
   if (name.length <= length + ellipsis.length) {
@@ -6,7 +6,8 @@ export function nameOmit(name: string, ellipsis = "...", length = 10): string {
   } else {
     nameRes = name.slice(0, length) + ellipsis;
   }
-  return nameRes.toUpperCase();
+  if (uppercase) return nameRes.toUpperCase();
+  return nameRes;
 }
 
 export default { nameOmit };
