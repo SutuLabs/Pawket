462649869b72c57eb60c0c8ab5942f1fe80296a1
Sun Haochen <sunhaochen@sutu.tech>
2022-05-05 23:32:20 -0700

    convert cat name to uppercase

--- src/filters/nameConversion.ts
+++ src/filters/nameConversion.ts
@@ -1,7 +1,12 @@
 export function nameOmit(name: string, ellipsis = "...", length = 10): string {
-  if (!name) return "";
-  if (name.length <= length + ellipsis.length) return name;
-  return name.slice(0, length) + ellipsis;
+  let nameRes = "";
+  if (!name) return nameRes;
+  if (name.length <= length + ellipsis.length) {
+    nameRes = name;
+  } else {
+    nameRes = name.slice(0, length) + ellipsis;
+  }
+  return nameRes.toUpperCase();
 }
 
 export default { nameOmit };
