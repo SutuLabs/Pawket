064d20ab62f656ba374245d25dc99053c0b62646
LiangShuang <liangshuang@sutu.tech>
2022-07-24 22:32:35 +0800

    auto execute solution in bundle

--- src/components/DevHelper/BundlePanel.vue
+++ src/components/DevHelper/BundlePanel.vue
@@ -214,7 +214,7 @@ export default class BundlePanel extends Vue {
       this.bundle = null;
     }
 
-    this.changeCoin(0);
+    await this.changeCoin(0);
     this.save();
   }
 
@@ -222,25 +222,26 @@ export default class BundlePanel extends Vue {
     return JSON.stringify(this.bundle, null, 4);
   }
 
-  mounted(): void {
+  async mounted(): Promise<void> {
     // console.log(this.beautifyLisp('((70 0xcfc57e87082a076c05acfe5c6f2e89c34e0bf0572e9bf278f0d4cfffcf149338) (60 0x5ebf47089c24e748881dc0eea96e8a94ea48e236526bde3fb293d99238fecab4) (61 0x92e3e2979754cecffd5d3aa3db521e4dab109c15523eb5940a69119ced36ec02) (50 0xb00d78ca128b87506a8e6b02cdadd1c4e5da26d2248f3df290c2fabf617a49becd40c8e8fb22db6df020e4f89fae756c 0xfcee98e201e03cf19329f4d65833cdf36c513e22c0df429d3a6c2954f6b60ec7) (51 0xdf4ec566122b8507fc920d03b04d7021909d5bcd58beed99710ba9bea58f970b 1000 (0xbae24162efbd568f89bc7a340798a6118df0189eb9e3f8697bcea27af99f8f79)) (51 0x9d9e5a5d1a5860f5c30b43123e9293d70123ba4264bea2e6304e5ccde7f41ba5 0x01068b) (63 0xc2e7416bf62c2d1a4c8c71ca34bf70603332ac19fe541ae64af6370b2a218ded))'));
     if (this.inputBundleText) {
       this.bundleText = this.inputBundleText;
-      this.updateBundle();
+      await this.updateBundle();
     } else {
-      this.load();
+      await this.load();
     }
+    await this.executePuzzle(this.puzzle, this.solution);
   }
 
   beautifyLisp(text: string): string {
     return beautifyLisp(text);
   }
 
-  load(): void {
+  async load(): Promise<void> {
     const bd = localStorage.getItem("BUNDLE_DEBUG");
     if (bd) {
       this.bundleText = bd;
-      this.updateBundle();
+      await this.updateBundle();
     }
   }
 
