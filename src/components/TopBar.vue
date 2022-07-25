099a8c24b6ecc8af4d52c6729184555f9c29c808
Sun Haochen <sunhaochen@sutu.tech>
2022-07-24 22:32:58 -0600

    fix devhelper network ui

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,7 +4,8 @@
       <span @click="$emit('close')"
         ><b-icon class="is-pulled-left has-text-grey pr-3 is-clickable" icon="chevron-left"> </b-icon
       ></span>
-      <span class="pr-5">{{ title }}</span>
+      <span class="pr-2">{{ title }}</span>
+      <b-tag type="is-info" v-if="tag">{{ tag }}</b-tag>
     </div>
     <div class="is-hidden-mobile modal-card-title">
       <span v-if="showBack" @click="$emit('close')"
@@ -14,6 +15,7 @@
         ><b-icon class="is-pulled-right has-text-grey is-clickable hover-danger" icon="close"> </b-icon
       ></span>
       <span class="pl-4">{{ title }}</span>
+      <b-tag type="is-info" class="ml-2" v-if="tag">{{ tag }}</b-tag>
     </div>
   </div>
 </template>
@@ -24,8 +26,9 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class TopBar extends Vue {
   @Prop() title!: string;
+  @Prop({ default: "" }) tag!: string;
   @Prop({ default: false }) showBack!: boolean;
-   @Prop({ default: false }) showClose!: boolean;
+  @Prop({ default: false }) showClose!: boolean;
 }
 </script>
 
