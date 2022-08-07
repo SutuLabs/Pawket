46213a3a3ec28d313dd408cabb7e2fc651b002b3
LiangShuang <liangshuang@sutu.tech>
2022-08-07 22:41:05 +0800

    collapse bugs into bug icon only in production

--- src/services/errorLog/errorLog.ts
+++ src/services/errorLog/errorLog.ts
@@ -2,18 +2,20 @@ import Vue from "vue";
 import store from "@/store";
 import { errorLog } from "@/store/modules/error";
 
-Vue.config.errorHandler = function (err: Error, vm: Vue, info: string): void {
-  Vue.nextTick(() => {
-    const errLog: errorLog = {
-      msg: err.message,
-      info: info,
-      url: window.location.href,
-      stack: err.stack ?? "",
-    };
-    store.dispatch("addErrorLog", errLog);
+if (process.env.NODE_ENV === "production") {
+  Vue.config.errorHandler = function (err: Error, vm: Vue, info: string): void {
+    Vue.nextTick(() => {
+      const errLog: errorLog = {
+        msg: err.message,
+        info: info,
+        url: window.location.href,
+        stack: err.stack ?? "",
+      };
+      store.dispatch("addErrorLog", errLog);
 
-    if (process.env.NODE_ENV !== "production") {
-      console.warn(err);
-    }
-  });
-};
+      if (process.env.NODE_ENV !== "production") {
+        console.warn(err);
+      }
+    });
+  };
+}
