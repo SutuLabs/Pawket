f2915f33b8b22f1323828b2b01c1ac6648b0c21e
LiangShuang <liangshuang@sutu.tech>
2022-10-03 09:16:25 +0800

    fix warning

--- src/components/Mint/MintNft.vue
+++ src/components/Mint/MintNft.vue
@@ -451,6 +451,7 @@ export default class MintNft extends Vue {
         this.royaltyPercentage * 100,
         did.analysis,
         getLineageProofPuzzle,
+        undefined,
         isCns
       );
 
