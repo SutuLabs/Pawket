3beb1226f64f3d57cd9696d98dd09b9e939aee23
LiangShuang <liangshuang@sutu.tech>
2022-11-01 22:56:22 +0800

    allow beautify lisp in clvm panel

--- src/components/DevHelper/ClvmPanel.vue
+++ src/components/DevHelper/ClvmPanel.vue
@@ -8,6 +8,9 @@
         Translated
         <key-box icon="checkbox-multiple-blank-outline" :value="translated_cl" tooltip="Copy"></key-box>
         <span v-if="modsdict[origin_cl]" class="tag is-info is-light is-small">{{ modsdict[origin_cl] }}</span>
+        <b-button v-if="cl_type == 'hex'" tag="a" size="is-small" @click="translated_cl = beautifyLisp(translated_cl)">
+          <b-icon icon="format-paint"></b-icon>
+        </b-button>
       </template>
       <b-input type="textarea" v-model="translated_cl" disabled></b-input>
       {{ cl_extra }}
@@ -27,7 +30,7 @@ import { Component, Vue } from "vue-property-decorator";
 import KeyBox from "@/components/Common/KeyBox.vue";
 import puzzle from "@/services/crypto/puzzle";
 import { modsdict } from "@/services/coin/mods";
-import { findByPath } from "@/services/coin/lisp";
+import { beautifyLisp, findByPath } from "@/services/coin/lisp";
 import { assemble, disassemble } from "clvm_tools";
 
 @Component({
@@ -66,6 +69,10 @@ export default class ClvmPanel extends Vue {
     const program = assemble(this.origin_cl);
     const ret = findByPath(program, this.navigator);
     this.navigateResult = disassemble(ret);
+  }
+
+  beautifyLisp(text: string): string {
+    return beautifyLisp(text);
   }
 }
 </script>
