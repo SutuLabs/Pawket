8bfd600a60cf364577ccd316bab4ae0f3e8d2d02
sunhaochen <sunhaochen@sutu.tech>
2022-11-13 22:25:24 -0700

    no-pawket-account page

--- src/components/Connect/Connect.vue
+++ src/components/Connect/Connect.vue
@@ -1,58 +1,65 @@
 <template>
   <div class="has-text-centered mt-6">
-    <div class="box mx-6 is-flex is-justify-content-center">
-      <img v-if="origin" :src="`${origin}/favicon.ico`" class="image is-24x24 mx-3" />{{ origin }}
-    </div>
-    <div class="is-size-3 has-text-weighted-bold mb-6">Connect With Pawket</div>
-    <div v-if="stage == 'Verify'">
-      <div>Enter your password to connect to the site</div>
-      <div class="field mx-5 my-3">
-        <input
-          class="input is-medium"
-          type="password"
-          v-model="password"
-          @keyup.enter="confirm()"
-          @input.enter="clearErrorMsg()"
-        />
+    <div v-if="hasAccount">
+      <div class="box mx-6 is-flex is-justify-content-center">
+        <img v-if="origin" :src="`${origin}/favicon.ico`" class="image is-24x24 mx-3" />{{ origin }}
       </div>
-      <p class="help is-danger is-size-6" v-if="!isCorrect">{{ $t("verifyPassword.message.error.incorrectPassword") }}</p>
-      <div class="has-text-info">Make sure you trust the site you connect</div>
-      <footer class="is-fixed-bottom py-4 px-4 has-background-white-ter">
-        <button class="button is-pulled-left" @click="close">Cancel</button>
-        <button class="button is-pulled-right is-primary" @click="confirm">Next</button>
-      </footer>
-    </div>
-    <div v-if="stage == 'Account'">
-      <div>Select an account to use on this site</div>
-      <div class="pb-10">
-        <div class="box m-3">
-          <a
-            v-for="(account, idx) in accounts"
-            :key="idx"
-            :class="{ 'panel-block': true, 'list-item': idx }"
-            @click="selectedAcc = idx"
-          >
-            <input type="radio" class="mr-2 has-text-primary" :checked="selectedAcc == idx" />
-            <figure class="image is-32x32" style="margin: auto">
-              <img v-if="account.profilePic" class="is-rounded cover" :src="account.profilePic" />
-              <img v-else class="is-rounded" src="@/assets/account-circle.svg" />
-            </figure>
-            <div class="column is-flex my-0 py-0">
-              <div class="py-1">
-                <p class="is-size-6">{{ account.name }}({{ account.key.fingerprint }})</p>
-                <p class="is-size-7 has-text-grey has-text-left" v-if="account.tokens && account.tokens.hasOwnProperty(symbol)">
-                  {{ demojo(account.tokens[symbol].amount) }}
-                </p>
-                <p class="is-size-7 has-text-grey has-text-left" v-else>Loading Balance..</p>
-              </div>
-            </div>
-          </a>
+      <div class="is-size-3 has-text-weighted-bold mb-6">Connect With Pawket</div>
+      <div v-if="stage == 'Verify'">
+        <div>Enter your password to connect to the site</div>
+        <div class="field mx-5 my-3">
+          <input
+            class="input is-medium"
+            type="password"
+            v-model="password"
+            @keyup.enter="confirm()"
+            @input.enter="clearErrorMsg()"
+          />
         </div>
+        <p class="help is-danger is-size-6" v-if="!isCorrect">{{ $t("verifyPassword.message.error.incorrectPassword") }}</p>
+        <div class="has-text-info">Make sure you trust the site you connect</div>
+        <footer class="is-fixed-bottom py-4 px-4 has-background-white-ter">
+          <button class="button is-pulled-left" @click="close">Cancel</button>
+          <button class="button is-pulled-right is-primary" @click="confirm">Next</button>
+        </footer>
       </div>
-      <footer class="is-fixed-bottom py-4 px-4 has-background-white-ter">
-        <button class="button is-pulled-left" @click="close()">Cancel</button>
-        <button class="button is-pulled-right is-primary" @click="openApp()">Connect</button>
-      </footer>
+      <div v-if="stage == 'Account'">
+        <div>Select an account to use on this site</div>
+        <div class="pb-10">
+          <div class="box m-3">
+            <a
+              v-for="(account, idx) in accounts"
+              :key="idx"
+              :class="{ 'panel-block': true, 'list-item': idx }"
+              @click="selectedAcc = idx"
+            >
+              <input type="radio" class="mr-2 has-text-primary" :checked="selectedAcc == idx" />
+              <figure class="image is-32x32" style="margin: auto">
+                <img v-if="account.profilePic" class="is-rounded cover" :src="account.profilePic" />
+                <img v-else class="is-rounded" src="@/assets/account-circle.svg" />
+              </figure>
+              <div class="column is-flex my-0 py-0">
+                <div class="py-1">
+                  <p class="is-size-6">{{ account.name }}({{ account.key.fingerprint }})</p>
+                  <p class="is-size-7 has-text-grey has-text-left" v-if="account.tokens && account.tokens.hasOwnProperty(symbol)">
+                    {{ demojo(account.tokens[symbol].amount) }}
+                  </p>
+                  <p class="is-size-7 has-text-grey has-text-left" v-else>Loading Balance..</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <footer class="is-fixed-bottom py-4 px-4 has-background-white-ter">
+          <button class="button is-pulled-left" @click="close()">Cancel</button>
+          <button class="button is-pulled-right is-primary" @click="openApp()">Connect</button>
+        </footer>
+      </div>
+    </div>
+    <div v-else>
+      <div class="is-size-3 has-text-weighted-bold">No Pawket Wallet</div>
+      <div class="my-6">You do not have a wallet set up in Pawket.</div>
+      <button class="button is-primary" @click="setUp()">Set up Wallet</button>
     </div>
   </div>
 </template>
@@ -107,6 +114,10 @@ export default class Connect extends Vue {
     return this.accounts[this.selectedAcc];
   }
 
+  get hasAccount(): boolean {
+    return localStorage.getItem("SETTINGS") != null;
+  }
+
   get networks(): NetworkInfo {
     return store.state.network.networks;
   }
@@ -140,6 +151,11 @@ export default class Connect extends Vue {
     const derive = await utility.derive(privkey, false);
     const firstWalletAddressPubkey = utility.toHexString(derive([12381, 8444, 2, 0]).get_g1().serialize());
     Vue.set(account, "firstAddress", await puzzle.getAddress(firstWalletAddressPubkey, this.prefix));
+  }
+
+  setUp(): void {
+    window.open(window.location.origin);
+    this.close();
   }
 
   async getXchBalance(account: AccountEntity): Promise<void> {
@@ -215,12 +231,6 @@ export default class Connect extends Vue {
   }
 
   success(): void {
-    Notification.open({
-      message: "Submitted Successfully!",
-      type: "is-primary",
-      position: "is-top",
-      duration: 5000,
-    });
     setTimeout(() => this.close(), 5000);
   }
 
