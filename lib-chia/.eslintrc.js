276d9cab2f5bcd805aade8b51eae8c5b4d33120f
LiangShuang <liangshuang@sutu.tech>
2023-12-06 23:43:15 +0800

    try set source type

--- lib-chia/.eslintrc.js
+++ lib-chia/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     ],
     parserOptions: {
       ecmaVersion: 2020,
+      sourceType: "module"
     },
     rules: {
       "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
