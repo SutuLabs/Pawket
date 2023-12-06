240f79db2572449002e47b6c1c7b3efa88c22600
LiangShuang <liangshuang@sutu.tech>
2023-12-06 23:28:42 +0800

    update eslint parser

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -9,11 +9,12 @@ module.exports = {
       "@vue/typescript/recommended",
       "@vue/prettier/@typescript-eslint",
     ],
+    parser: "vue-eslint-parser",
     parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module"
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: 2020,
+        sourceType: "module"
     },
-    parser: "@typescript-eslint/parser",
     rules: {
       "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
       "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
