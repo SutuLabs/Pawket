45a72dd44784b43fae15c172aec8cb9f5958869f
LiangShuang <liangshuang@sutu.tech>
2023-12-06 21:51:11 +0800

    fix build issue

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     ],
     parserOptions: {
       ecmaVersion: 2020,
+      sourceType: "module"
     },
     rules: {
       "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
