1c87f0c5d1e58608245920180a0044d96151763f
Icer Liang <liangshuang@sutu.tech>
2024-10-10 22:25:45 +0800

    update mixch home

--- ui/src/components/DevHelper/MixchHome.vue
+++ ui/src/components/DevHelper/MixchHome.vue
@@ -8,7 +8,8 @@
     <p>
       <b>
         This toolkit has been built to assist the development of
-        <a target="_blank" href="https://pawket.app">Pawket Wallet</a> and Chia Name Service.
+        <a target="_blank" href="https://pawket.app">Pawket Wallet</a> and
+        <a target="_blank" href="https://xch.cool">Chia Name Service</a>.
       </b>
       <br />
       With this toolkit, you can play around with different schemes and algorithms to understand how they work. Or you can create
@@ -28,6 +29,21 @@
       <li><router-link :to="{ name: 'Simulator' }">CLVM Simulator</router-link></li>
       <li><router-link :to="{ name: 'CoinDeveloper' }">Chialisp Coin Studio</router-link></li>
     </ul>
+    <h2>Donations</h2>
+    <p>
+      If you find this toolkit useful, please consider donating to support the development and maintenance of this project. Thank
+      you!
+      <br />
+      Donation Address:
+      <key-box
+        :value="`xch16xwqtf2d4jljks8lfpp4xnz8jak7jqjxc07y9tqlgt4grdp5hr4q7vcx3s`"
+        display="xch16xwqtf2d4jljks8lfpp4xnz8jak7jqjxc07y9tqlgt4grdp5hr4q7vcx3s"
+        icon="checkbox-multiple-blank-outline"
+      />
+      (
+      <a target="_blank" href="https://pawket.xch.cool/">pawket.xch</a>
+      )
+    </p>
     <p class="is-pulled-right">by <a target="_blank" href="https://twitter.com/icerdesign">@icerdesign</a></p>
   </div>
 </template>
