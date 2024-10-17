ca852898114f76726ee451947e02b135e88833cf
Icer Liang <liangshuang@sutu.tech>
2024-10-17 22:39:03 +0800

    Revert "allow to submit bundle to network"
    This reverts commit 4fe937cbc55bf8af7a816d533c3844d33ee7e42e.

--- ui/src/services/view/bundleAction.ts
+++ ui/src/services/view/bundleAction.ts
@@ -11,7 +11,7 @@ import { Hex, Hex0x, prefix0x } from "../../../../lib-chia/services/coin/conditi
 
 export async function submitBundle(
   bundle: SpendBundle,
-  account: AccountEntity | undefined,
+  account: AccountEntity,
   setSubmitting: (state: boolean) => void,
   success: () => void
 ): Promise<void> {
@@ -49,9 +49,7 @@ export async function submitBundle(
         type: "is-primary",
       });
       const txnTime = Date.now();
-      if (account) {
-        lockCoins(account, bundle.coin_spends, txnTime, chainId());
-      }
+      lockCoins(account, bundle.coin_spends, txnTime, chainId());
       success();
     } else {
       const err = typeof json.error === "string" ? json.error.match("error ([A-Z_]+)") : null;
